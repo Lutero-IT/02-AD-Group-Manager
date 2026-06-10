66eaa21d4d8e650c4ccef69599a78af4633564f3
Lutero-IT <lutero.it@gmail.com>
2026-06-10 17:46:47 +0200

    feat: Add group validation
    When operator provides a group, it is searched accross Active Directory
    and if not found, corresponding message is shown and user is asked
    to pass valid AD group name or exit the program.

--- Manage-ADGroupMembers.ps1
+++ Manage-ADGroupMembers.ps1
@@ -1,3 +1,13 @@
+### IMPORTANT ###
+<#
+!!! TO KEEP IN MIND !!!
+    KEEP THE PROGRAM AS SIMPLE AS POSSIBLE!
+    DO NOT COMPLICATE IT AND DO NOT OVERDO IT!
+
+WHEN YOU WRITE LOGIC AND DO BASIC STYLING, THEN THINK ABOUT DEVELOPING PROGRAM!
+#>
+
+
 # Functions created to change classical Write-Host and Read-Host behaviour
 # by adding indentation in the beginning of each message and newline below it
 # for better readability
@@ -6,47 +16,74 @@ $Indent = "`t"
 
 function Write-IndentedLog ($Message) {
     Write-Host "${Indent}$Message" @Args
-    Write-Host ""
 }
 
 function Read-IndentedLog ($Message) {
     Read-Host "${Indent}$Message"
+    Write-Host ""
 }
 
 # PREDIFINIED WHITELISTS #
 $exitOptions = "exit e" -split" "
-Write-Host $exitOptions
-Write-Host $exitOptions.GetType()
 
+# STARTING MENU #
+$title = "Active Directory Group Member Manager"
+
+# ================== #
+
+Write-IndentedLog ("="*80)
+Write-IndentedLog (" " * ((80 - [int]$title.Length) / 2 ) ) $title
+Write-IndentedLog ("="*80)
+
+Write-Host ""
 Write-IndentedLog "Welcome in Group Member Manager!"
 Write-IndentedLog "In this program you can add, remove or get members of any group"
 Write-IndentedLog "present in your Active Directory database."
 Write-IndentedLog "To start, provide name of a an AD group you wish to modify"
 
+# FIRST LOOP #
 while ($true) {
     $groupName = Read-IndentedLog
 
+<# CODE TO TEST INPUT - delete in final version of the script!!!
     Write-Host "The input: $groupname"
     Write-Host "The type: " $groupname.GetType()
     Write-Host "The length: " $groupName.Length
-
+#>
     if ($groupName.Length -eq 0) {
-        Write-IndentedLog "No input passed" # style it to look like warning
-        Write-IndentedLog "Pass valid AD group name or..."
-        Write-IndentedLog "...exit the program by typing [Exit/E] (case insesitive)"
+        Write-IndentedLog "No input passed" -BackgroundColor Red -ForegroundColor White
+        Write-IndentedLog "Pass valid AD group name or exit the program by typing [Exit/E] (case insensitive)"
     } elseif ($groupName -in $exitOptions) {
-        Write-IndentedLog "Chose to exit the program"
+        Write-IndentedLog "Chose to exit the program" -BackgroundColor Yellow -ForegroundColor Black
         break
     } else {
-        Write-IndentedLog "Chose to continue..."
-        break
+        Write-IndentedLog "Checking if group exists in AD database..."
+        $groupExists = [bool](Get-ADGroup -Filter "Name -eq '$groupName'")
+            if ($groupExists) {
+                Write-IndentedLog "$groupName found in Active Directory!" -BackgroundColor Green -ForegroundColor White
+                break
+            } else {
+                Write-IndentedLog "Group $groupName doesn't exist in Active Directory!" -BackgroundColor Red -ForegroundColor White
+                Write-IndentedLog "Pass valid AD group name or exit the program by typing [Exit/E] (case insensitive)"
+            }        
     }
 }
-Write-IndentedLog "Program closed"
+
+# CHECK IF GROUP EXISTS IN DATABASE #
+
+
+
+Write-IndentedLog "Program closed" -BackgroundColor Blue -ForegroundColor White
+
+# MAIN MENU #
+
 
 <# TO DO:
+
 Write while loop that will check wheher $groupName:
-1. is not empty. If it is, inform the user and repeat the request or offer to exit the program.
 2. is a valid group name, meaning if the group exists in database. If not, inform the user
-and repeat the request or offer exit option.
-3. if it is valid, proceed with the program and present main menu. #>
+and repeat the request or offer exit option. - DO COMMIT
+2a. In addition to prompt for a group name or exit option, offer to list all the groups in Active Directory
+to user to choose from.
+3. if it is valid, proceed with the program and present main menu. 
+#>
